e2bc95ddd9451084dbdd2af73dad5610a999f3b8
daisukiyo <tk.rain09@gmail.com>
2019-04-07 23:23:07 -0700

    complete search assignment

--- Lessons/source/search.py
+++ Lessons/source/search.py
@@ -34,22 +34,46 @@ def binary_search(array, item):
     """return the index of item in sorted array or None if item is not found"""
     # implement binary_search_iterative and binary_search_recursive below, then
     # change this to call your implementation to verify it passes all tests
-    return binary_search_iterative(array, item)
-    # return binary_search_recursive(array, item)
+    # return binary_search_iterative(array, item)
+    return binary_search_recursive(array, item)
 
 
 def binary_search_iterative(array, item):
     # TODO: implement binary search iteratively here
-    pass
+    left = 0
+    right = len(array) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+        if item == array[mid]:
+            return(mid)
+        elif item < array[mid]:
+            right = mid - 1
+        else:
+            left = mid + 1
+    return None
+
     # once implemented, change binary_search to call binary_search_iterative
     # to verify that your iterative implementation passes all tests
 
 
 def binary_search_recursive(array, item, left=None, right=None):
     # TODO: implement binary search recursively here
-    pass
+    if left == None: 
+        left = 0
+    if right == None:
+        right = len(array) -1
+
+    if left > right:
+        return None
+    else:
+        mid = (left + right) // 2
+        if item == array[mid]:
+            return(mid)
+        elif item < array[mid]:
+            return binary_search_recursive(array, item, left, mid-1)
+        else:
+            return binary_search_recursive(array, item, mid+1, right)
+
     # once implemented, change binary_search to call binary_search_recursive
     # to verify that your recursive implementation passes all tests
-
-myArray = ['Winnie', 'Kojin', 'Brian', 'Nabil', 'Julia', 'Alex', 'Nick']
-print(linear_search_recursive(myArray, 'Nick', index=0))
